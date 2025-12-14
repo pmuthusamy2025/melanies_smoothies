d09a7ec3af8e438e200acf3d140e138976de40e1
pmuthusamy2025 <pmuthusamy@harbourvest.com>
2025-12-14 18:59:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ ingredients_list = st.multiselect(
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 #st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),user_container_width=True)
+sf_df = st.dataframe(data=smoothiefroot_response.json())
 
 if ingredients_list:  
     ingredients_string = ''
