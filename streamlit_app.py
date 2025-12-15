d8e98487774c6f5b574b81dd2d6db3bd31930dab
pmuthusamy2025 <pmuthusamy@harbourvest.com>
2025-12-14 19:25:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 # Convert Dataframe to Panda
 pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
